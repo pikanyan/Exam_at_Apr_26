5f6e2162b56788d1dd8185108ed3ce0b13500628
pikanyan <113081175+pikanyan@users.noreply.github.com>
2024-04-25 23:28:47 +0900

    update

---
+++ 02 scanf and printf character/main.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	char input_character;
+
+	scanf("%c", &input_character);		// input character
+	printf("%c\n", input_character);	// output character
+
+
+
+	return 0;
+}
