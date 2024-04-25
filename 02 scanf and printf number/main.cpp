96dd753b4a5e4a62c84d6eb921e7280efa3875e8
pikanyan <113081175+pikanyan@users.noreply.github.com>
2024-04-25 23:27:48 +0900

    update

---
+++ 02 scanf and printf number/main.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	int input_number;
+
+	scanf("%d", &input_number);		// input number
+	printf("%d\n", input_number);	// output number
+
+
+
+	return 0;
+}
